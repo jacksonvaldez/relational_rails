4d1addec27ffff95ad7b7d2468103576cd28c0fa
Jackson Valdez <jacksonvaldez64@gmail.com>
2021-11-29 19:31:22 -0700

    Start on db migrater and schema

---
+++ db/migrate/20211130015950_create_task.rb
@@ -0,0 +1,11 @@
+class CreateTask < ActiveRecord::Migration[5.2]
+  def change
+    create_table :artists do |t|
+      t.string :name
+      t.boolean :alive
+      t.integer :monthly_listeners
+      t.timestamp :created_at
+      t.timestamp :updated_at
+    end
+  end
+end
