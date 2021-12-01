fda6a444b48b1cbc61ffe9bde0f6c5d7f869869e
Jackson Valdez <jacksonvaldez64@gmail.com>
2021-11-30 17:27:09 -0700

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   get '/', to: 'welcome#index'
+
+  # Artist/Song Relationship
   get '/artists', to:'artists#index'
   get '/artists/:id', to: 'artists#show'
+
+  # Bakery/Baker Relationship
+  get '/bakeries', to:'bakeries#index'
+  get 'bakeries/:id', to:'bakeries#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
