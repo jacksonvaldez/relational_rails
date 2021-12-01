50f98f5764ea306a0d0441308fd6fa4b13b8f2f8
Arnaldo Aparicio <apaarn2211@gmail.com>
2021-11-30 22:11:42 -0500

    Add methods that return associated children to parent models

--- app/models/bakery.rb
+++ app/models/bakery.rb
@@ -1,3 +1,12 @@
 class Bakery < ApplicationRecord
 
+  def bakers
+    # binding.pry
+    Baker.all.to_a.find_all do |baker|
+      # binding.pry
+      baker.bakery_id == self.id
+
+    end
+  end
+
 end
