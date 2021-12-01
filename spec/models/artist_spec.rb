e27076b78c0a3bbe69ae68944214c1e1c9b52fcb
Jackson Valdez <jacksonvaldez64@gmail.com>
2021-12-01 16:39:36 -0700

    Complete tests for parent models

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -1,14 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Artist do
-  describe 'instance methods' do
-    it 'attributes' do
-      artist = Artist.new({ name: 'Prince', alive: false, monthly_listeners: 50_000 })
-      expect(artist.name).to eq('Prince')
-      expect(artist.alive).to eq(false)
-      expect(artist.monthly_listeners).to eq(50000)
-      binding.pry
-      # expect(artist.created_at).to be_an(50000)
+  before(:each) do
+    @artist = Artist.create!({ name: 'Prince', alive: false, monthly_listeners: 50_000 })
+    @song_1 = Song.create!(name: 'Purple Rain', top_100: false, length_s: 100, artist_id: @artist.id)
+    @song_2 = Song.create!(name: 'Dove Cry', top_100: true, length_s: 150, artist_id: @artist.id)
+  end
+  it 'attributes' do
+    expect(@artist.name).to eq('Prince')
+    expect(@artist.alive).to eq(false)
+    expect(@artist.monthly_listeners).to eq(50000)
+    expect(@artist.created_at).to be_a(ActiveSupport::TimeWithZone)
+    expect(@artist.updated_at).to be_a(ActiveSupport::TimeWithZone)
+    # expect(artist.created_at).to be_an(50000)
+  end
+
+  describe '#songs' do
+    it 'returns a collection of songs' do
+      # binding.pry
+      expect(@artist.songs).to eq([@song_1, @song_2])
+    end
+  end
+
+  describe '#song_count' do
+    it 'returns the count of songs that the artist has' do
+      expect(@artist.song_count).to eq(2)
     end
   end
 end
---
+++ spec/models/bakery_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe Bakery do
+  before(:each) do
+    @bakery = Bakery.create!({ name: 'Dunkin Donutes', is_open: false, customer_capacity: 20 })
+    @baker_1 = Baker.create!(name: 'jon smith', is_working: false, salary: 10000, bakery_id: @bakery.id)
+    @baker_2 = Baker.create!(name: 'andrew jackson', is_working: true, salary: 15000, bakery_id: @bakery.id)
+  end
+  it 'attributes' do
+    expect(@bakery.name).to eq('Dunkin Donutes')
+    expect(@bakery.is_open).to eq(false)
+    expect(@bakery.customer_capacity).to eq(20)
+    expect(@bakery.created_at).to be_a(ActiveSupport::TimeWithZone)
+    expect(@bakery.updated_at).to be_a(ActiveSupport::TimeWithZone)
+    # expect(artist.created_at).to be_an(50000)
+  end
+
+  describe '#bakers' do
+    it 'returns a collection of bakers' do
+      # binding.pry
+      expect(@bakery.bakers).to eq([@baker_1, @baker_2])
+    end
+  end
+
+  describe '#baker_count' do
+    it 'returns the count of bakers that the artist has' do
+      expect(@bakery.baker_count).to eq(2)
+    end
+  end
+end
