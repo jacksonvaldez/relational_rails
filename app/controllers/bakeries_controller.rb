202ae4852273dcbf50aa1082a938ea685d6e744c
Arnaldo Aparicio <apaarn2211@gmail.com>
2021-12-02 17:25:08 -0500

    Add new and create methods

--- app/controllers/bakeries_controller.rb
+++ app/controllers/bakeries_controller.rb
@@ -9,4 +9,17 @@ class BakeriesController < ApplicationController
     @bakery = Bakery.find(params[:id])
   end
 
+  def new
+  end
+
+  def create
+    params[:bakery][:is_open] == "true" ? is_open = true : is_open = false
+    Bakery.create(
+      name: params[:bakery][:name],
+      is_open: is_open,
+      customer_capacity: params[:bakery][:customer_capacity].to_i
+    )
+    redirect_to '/bakeries/'
+  end
+
 end
