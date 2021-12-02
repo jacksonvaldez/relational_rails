958cd821176f837be25607cd33742b15e5b63047
Arnaldo Aparicio <apaarn2211@gmail.com>
2021-12-02 17:30:03 -0500

    Add new routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,22 @@ Rails.application.routes.draw do
 
   # Artist/Song Relationship
   get '/artists', to:'artists#index'
+  get '/artists/new', to: 'artists#new'
   get '/artists/:id', to: 'artists#show'
   get '/songs', to:'songs#index'
   get '/songs/:id', to:'songs#show'
   get '/artists/:id/songs', to: 'songs#show_by_artist'
 
+  post '/artists/', to: 'artists#create'
+
   # Bakery/Baker Relationship
   get '/bakeries', to:'bakeries#index'
+  get '/bakeries/new', to: 'bakeries#new'
   get '/bakeries/:id', to:'bakeries#show'
   get '/bakers', to:'bakers#index'
   get '/bakers/:id', to:'bakers#show'
   get '/bakeries/:id/bakers', to:'bakers#show_by_bakery'
+
+  post '/bakeries/', to: 'bakeries#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
