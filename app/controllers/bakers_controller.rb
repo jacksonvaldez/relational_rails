d47ede6f884495a0ab8dc6f6ecea4efde84f8bb0
Jackson Valdez <jacksonvaldez64@gmail.com>
2021-12-02 17:24:37 -0700

    add new and create methods to child controllers

--- app/controllers/bakers_controller.rb
+++ app/controllers/bakers_controller.rb
@@ -10,8 +10,23 @@ class BakersController < ApplicationController
   end
 
   def show_by_bakery
-    bakery = Bakery.find(params[:id])
-    @bakers = bakery.bakers
+    @bakery = Bakery.find(params[:id])
+    @bakers = @bakery.bakers
+  end
+
+  def new
+    @bakery = Bakery.find(params[:id])
+  end
+
+  def create
+    params[:is_working] == "true" ? is_working = true : is_working = false
+    Baker.create({
+      name: params[:baker][:name],
+      is_working: is_working,
+      salary: params[:baker][:salary].to_i,
+      bakery_id: params[:id]
+    })
+    redirect_to "/bakeries/#{params[:id]}/bakers"
   end
 
 end
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -10,8 +10,23 @@ class SongsController < ApplicationController
   end
 
   def show_by_artist
-    artist = Artist.find(params[:id])
-    @songs = artist.songs
+    @artist = Artist.find(params[:id])
+    @songs = @artist.songs
+  end
+
+  def new
+    @artist = Artist.find(params[:id])
+  end
+
+  def create
+    params[:top_100] == "true" ? top_100 = true : top_100 = false
+    Song.create({
+      name: params[:song][:name],
+      top_100: top_100,
+      length_s: params[:song][:length_s].to_i,
+      artist_id: params[:id]
+    })
+    redirect_to "/artists/#{params[:id]}/songs"
   end
 
 end
