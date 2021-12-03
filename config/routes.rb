f1e0d103868e13652dd6660d398777675f489e87
Jackson Valdez <jacksonvaldez64@gmail.com>
2021-12-02 17:25:41 -0700

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,11 @@ Rails.application.routes.draw do
   get '/songs', to:'songs#index'
   get '/songs/:id', to:'songs#show'
   get '/artists/:id/songs', to: 'songs#show_by_artist'
+  get "/artists/:id/songs/new", to: 'songs#new'
 
   post '/artists/', to: 'artists#create'
   patch '/artists/:id', to:  'artists#update'
+  post "/artists/:id/songs", to: 'songs#create'
 
   # Bakery/Baker Relationship
   get '/bakeries', to:'bakeries#index'
@@ -21,8 +23,10 @@ Rails.application.routes.draw do
   get '/bakers', to:'bakers#index'
   get '/bakers/:id', to:'bakers#show'
   get '/bakeries/:id/bakers', to:'bakers#show_by_bakery'
+  get "/bakeries/:id/bakers/new", to: 'bakers#new'
 
   post '/bakeries/', to: 'bakeries#create'
   patch '/bakeries/:id', to: 'bakeries#update'
+  post "/bakeries/:id/bakers", to: 'bakers#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
