954f05b6c1caaf109e05eda1ef2f667df9965b33
Arnaldo Aparicio <apaarn2211@gmail.com>
2021-12-03 19:08:16 -0500

    Add tests for children deletion

--- spec/features/bakers/show_spec.rb
+++ spec/features/bakers/show_spec.rb
@@ -1,16 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe 'list of all bakers' do
+  before(:each) do
+    @bakery_1 = Bakery.create!(name: "Dunkin Donuts", is_open: true, customer_capacity: 100000)
+    @baker_1 = Baker.create!(name: "John Smith", is_working: true, salary: 300, bakery_id: @bakery_1.id)
+    @baker_2 = Baker.create!(name: "Jane Smith", is_working: false, salary: 250, bakery_id: @bakery_1.id)
+
+    visit "/bakers/#{@baker_1.id}"
+  end
   it 'list baker names' do
-    bakery_1 = Bakery.create!(name: "Dunkin Donuts", is_open: true, customer_capacity: 100000)
-    baker_1 = Baker.create!(name: "John Smith", is_working: true, salary: 300, bakery_id: bakery_1.id)
-    baker_2 = Baker.create!(name: "Jane Smith", is_working: false, salary: 250, bakery_id: bakery_1.id)
-
-    visit "/bakers/#{baker_1.id}"
-
     expect(page).to have_content('John Smith')
     expect(page).to have_content('Dunkin Donuts')
     expect(page).to have_content('true')
     expect(page).to have_content('300')
+  end
+
+  it 'redirects to correct path' do
+    expect(page).to have_current_path("/bakers/#{@baker_1.id}")
+  end
+
+  it 'redirects to correct path after deleting' do
+    click_on 'Delete'
+    expect(page).to have_current_path("/bakers")
+  end
+
+  it 'deletes a baker' do
+    click_on 'Delete'
+    expect(page).to have_content("Jane Smith")
+    expect(page).to_not have_content("John Smith")
   end
 end
--- spec/features/songs/show_spec.rb
+++ spec/features/songs/show_spec.rb
@@ -1,16 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe 'list of all songs' do
+  before(:each) do
+    @artist_1 = Artist.create!(name: "Kanye West", alive: true, monthly_listeners: 100000)
+    @song_1 = Song.create!(name: "Flashing Lights", top_100: true, length_s: 300, artist_id: @artist_1.id)
+    @song_2 = Song.create!(name: "Gold Digger", top_100: false, length_s: 250, artist_id: @artist_1.id)
+    visit "/songs/#{@song_1.id}"
+  end
+
   it 'list song names' do
-    artist_1 = Artist.create!(name: "Kanye West", alive: true, monthly_listeners: 100000)
-    song_1 = Song.create!(name: "Flashing Lights", top_100: true, length_s: 300, artist_id: artist_1.id)
-    song_2 = Song.create!(name: "Gold Digger", top_100: false, length_s: 250, artist_id: artist_1.id)
-
-    visit "/songs/#{song_1.id}"
-
     expect(page).to have_content('Flashing Lights')
     expect(page).to have_content('Kanye West')
     expect(page).to have_content('true')
     expect(page).to have_content('300')
+  end
+
+  it 'redirects to correct path' do
+    expect(page).to have_current_path("/songs/#{@song_1.id}")
+  end
+
+  it 'redirects to correct path after deleting' do
+    click_on 'Delete'
+    expect(page).to have_current_path("/songs")
+  end
+
+  it 'deletes a song' do
+    click_on 'Delete'
+    expect(page).to have_content("Gold Digger")
+    expect(page).to_not have_content("Flashing Lights")
   end
 end
